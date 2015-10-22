c46404a17f0b74e1aff652b34dc7da2ba0e3b8ce
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-21 22:52:31 -0700

    Described part 1 challenge

---
+++ js/promises.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  /*
+  In this challenge, you'll learn how to use promises to make independent requests as well as how to make dependendent requests
+
+  Part 1: Return what the current weather is based on a user's location (you can't use the location API though ;)
+
+  - When you click the Weather button, the current weather should be shown in the #weather-results section
+  - Use http://www.ip-api.com/json/ to get a user's location. This uses your IP address to get an estimate of your location
+  - With the result of that request, make another request to determine the current weather
+    - Use http://openweathermap.org/current
+    - The API is really straightforward, but you'll have to pass an additional parameter to getJSON in order to get it working!
+      - Take a look at jQuery's getJSON() API to see what that extra parameter is! http://api.jquery.com/jquery.getjson/
+   */
+});
